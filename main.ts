8187b6b390cb27b2776f30894ae613c4dea398b2
Lukas Petry <git.luke@rennwurst.de>
2022-10-24 01:55:25 +0200

    Make prefix configurable

--- main.ts
+++ main.ts
@@ -3,11 +3,11 @@ import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	classPrefix: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
+	classPrefix: "theme-",
 };
 
 export default class MyPlugin extends Plugin {
@@ -54,7 +54,8 @@ export default class MyPlugin extends Plugin {
 			.toLowerCase()
 			.replace(" ", "");
 
-		const newThemeClassName = "theme-" + selectedThemeSanitized;
+		const newThemeClassName =
+			this.settings.classPrefix + selectedThemeSanitized;
 
 		this.removeCssClass();
 		this.themeClassName = newThemeClassName;
@@ -84,16 +85,16 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
+			.setName("Prefix")
+			.setDesc("Prefix of the attached css class")
 			.addText((text) =>
 				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
+					.setPlaceholder("Enter your Prefix")
+					.setValue(this.plugin.settings.classPrefix)
 					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
+						this.plugin.settings.classPrefix = value;
 						await this.plugin.saveSettings();
+						this.plugin.updateCssClass();
 					})
 			);
 	}
